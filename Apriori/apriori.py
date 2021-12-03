8e79906f064d736c85e081e0da9c7ac69cea96f9
karthikml-code <85920192+karthikml-code@users.noreply.github.com>
2021-12-03 21:08:00 +0530

    Update apriori.py

--- Apriori/apriori.py
+++ Apriori/apriori.py
@@ -12,7 +12,7 @@ for i in range(0, 7501):
     transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
 
 # Training the Apriori model on the dataset
-from apyori import apriori
+from apyori1 import apriori
 rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
 
 # Visualising the results
